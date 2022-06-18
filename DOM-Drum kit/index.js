3fdbdd178f23f48714087a25f2437e59809dd1e9
PriyanshuGajbhiye <g.priyanshu1212@gmail.com>
2022-06-18 10:38:57 +0530

    Update index.js

--- DOM-Drum kit/index.js
+++ DOM-Drum kit/index.js
@@ -14,15 +14,57 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 }
 */
 
+//Detecting button press
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click",function(){
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
 
-      this.style.color = "white";     //this keyword is  used to figure out the identity, in this case identity of the button that triggered the EventListener i.e., button element
-    });
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+  });
 }
 
+// Detecting keyboard press
+//calling a fn with parameter event or e will give the event that triggered the function
+document.addEventListener("keydown",function(event){
 
-/*var audio = new Audio("sounds/tom-1.mp3");      new Audio is a method for playing audio inside we gave the url and then varible.play()  play is a method 
-audio.play();*/
+  makeSound(event.key);
+
+});
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3"); //new Audio is a method for playing audio inside we gave the url and then varible.play()  play is a method
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default:
+      consolg.log(buttonInnerHTML);
+  }
+}
