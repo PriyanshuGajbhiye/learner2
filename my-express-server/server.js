d08450ebd39b387d87d512ef716d49811aece4e8
PriyanshuGajbhiye <g.priyanshu1212@gmail.com>
2022-06-24 03:20:26 +0530

    Update server.js

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -11,8 +11,20 @@ app.get("/", function(request,response){
 */
 
 app.get("/", function(req,res){
-  res.send("Hello");           //The browser will go to location specified and will make request to get some data back and when that request get made callback gets triggered and send the browser a response that renders on our screen.
+  res.send("<h1>Hello</h1>");           //The browser will go to location specified and will make request to get some data back and when that request get made callback gets triggered and send the browser a response that renders on our screen.
 });                            //instead of text u can send back HTML too "<h1>Hello</h1>" It ill render Hello as h1 text.
+
+app.get("/contact", function(req,res){
+  res.send("Contact me at g.priyanshu1212@gmail.com");
+});
+
+app.get("/about", function(req,res){
+  res.send("I am priyanshu, a Front End Web Developer");
+});
+
+app.get("/hobbies",function(req,res){
+  res.send("<ul><li>Code</li><li>Gaming</li><li>cricket</li></ul>");
+});
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
