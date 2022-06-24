e245ea76cfda69eca6b9efe4469b7d406eee5c53
PriyanshuGajbhiye <g.priyanshu1212@gmail.com>
2022-06-24 19:09:27 +0530

    update calculator.js

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -5,8 +5,10 @@ const express = require("express");
 const app = express();
 
 app.get("/",function(req,res){
-  res.send("Hello World");
+  //console.log(__dirname);             //to check what it gives
+  res.sendFile(__dirname + "/index.html");    //for sending a complete file we have to give the absolute path not the relative path
 });
+//res.send just send a bits of individual html code
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
